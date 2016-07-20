a3526728e30e86a6db9f7b85cb3099c855fe5553
Daniel Fonseca <dfonseca@mpagg.local>
2016-07-20 14:59:45 -0300

    Adiciona exemplo de test usando Moq

---
+++ DIContainer_SimpleInjector/DIContainer.Tests/CharacterManagerTest.cs
@@ -0,0 +1,25 @@
+﻿using DIContainer.Business;
+using DIContainer.Repository.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace DIContainer.Tests {
+
+    [TestClass]
+    public class CharacterManagerTest {
+
+        [TestCategory("UT.DIContainer.Business.CharacterManager")]
+        [TestMethod]
+        public void GetSkillsForCharacter_Repository_Called() {
+            // Arrange
+            var mockRepository = new Mock<ICharacterSkillsRepository>();
+            var characterManager = new CharacterManager(mockRepository.Object);
+
+            // Act
+            characterManager.GetCharacterSkills(4);
+
+            // Assert
+            mockRepository.Verify( m => m.GetSkillsForCharacter(It.IsAny<int>()), Times.Once);
+        }
+    }
+}
---
+++ DIContainer_SimpleInjector/DIContainer.Tests/InitializeClass.cs
@@ -0,0 +1,25 @@
+﻿using DIContainer.Business;
+using DIContainer.Business.Interfaces;
+using DIContainer.Repository;
+using DIContainer.Repository.Interfaces;
+using DIContainer.SimpleInjector;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleInjector;
+
+
+namespace DIContainer.Tests {
+
+    [TestClass]
+    public class InitializeClass {
+
+        [AssemblyInitialize]
+        public static void Initialize(TestContext testContext)
+        {
+            var container = DependencyInjection.GetContainerInstance();
+
+            container.Register<ICharacterSkillsRepository, CharacterSkillsRepository>(Lifestyle.Singleton);
+            container.Register<ICharacterManager, CharacterManager>(Lifestyle.Singleton);
+        }
+
+    }
+}
